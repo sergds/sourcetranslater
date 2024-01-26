a449201a4d97d6de5f131ea47d7e47bf84538ea8
Sergey Morozyuk (AT Multiserver) <git@sergds.ga>
2024-01-26 23:28:53 +0300

    [server automation] easily support other python interpreters

--- tools/srctr_unattended.sh
+++ tools/srctr_unattended.sh
@@ -3,8 +3,12 @@
 # Translate on a server without needing an interactive ssh session. Also get a notification on finish.
 # Just run this script with a path to the game, basedir and a ntfy.sh topic name and disown it.
 
+if [ -z "$PYTHON" ]; then # on a multiserver i actually use pypy3.
+PYTHON="python3"
+fi
+
 # An example for l4d2:
-# bash tools/srctr_unattended.sh ~/l4d2 left4dead2 yourntfytopic & disown
+# PYTHON=pypy3 bash tools/srctr_unattended.sh "/home/sergds/Left 4 Dead 2" left4dead2 mycoolntfytopic & disown
 # Can also be launched from a custom systemd unit, init script, ci/cd pipeline, etc. for convinience.
 
 curl \
@@ -14,7 +18,7 @@ curl \
   -H "Tags: carrot,arrows_counterclockwise,wheelchair,srctr" \
   https://ntfy.sh/$3
 
-python3 sourcetranslater.py --lang russian --rounds 5 --installdir $1 --basedir $2 -N &> srctr_unattended.log
+$PYTHON sourcetranslater.py --lang russian --rounds 5 --installdir "`echo $1`" --basedir "`echo $2`" -N &> srctr_unattended.log
 curl \
   -d "tools/srctr_unattended.sh: Finished translating $2 on `hostname`" \
   -H "Title: SourceTranslater unattended translation" \
